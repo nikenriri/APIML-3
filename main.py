667e25ba3352de1ea4377a8bb14a484b37a4a566
niken riri <72871424+nikenriri@users.noreply.github.com>
2023-06-07 17:00:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -35,5 +35,5 @@ async def predict_fastapi(file: UploadFile = File(...)):
     return prediction
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=3000)
 
