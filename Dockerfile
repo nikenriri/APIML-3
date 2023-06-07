6ef8a981cac4bfdd286302b96f8047790561c51f
Niken Riri <nikenriri05@gmail.com>
2023-06-07 13:19:32 +0700

    Third commit

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM python:3.10.8
+
+WORKDIR /app
+
+COPY requirements.txt requirements.txt
+
+RUN pip install -r requirements.txt
+
+COPY mvp_model.h5 mvp_model.h5
+
+COPY main.py main.py
+
+ENV PYTHONUNBUFFERED=1 
+
+ENV HOST 0.0.0.0
+
+EXPOSE 8000
+
+CMD [ "python", "main.py" ]
---
+++ main.py
@@ -0,0 +1,39 @@
+from io import BytesIO
+import numpy as np
+import tensorflow as tf
+from fastapi import FastAPI,File,UploadFile
+import uvicorn
+from PIL import Image
+
+
+#Define Function
+labels = ['Jamur Enoki', 'Jamur Shimeji Coklat', 'Jamur Shimeji Putih', 'Jamur Tiram']
+
+def process(file)-> Image.Image:
+    image = image = Image.open(BytesIO(file))
+    return image
+
+def predict(image: Image.Image):
+    loaded_model = tf.keras.models.load_model('mvp_model.h5')
+    image = tf.image.resize(np.array(image), (224,224))
+    image = Image.fromarray(np.uint8(image.numpy()))
+    image = image.convert("RGB")
+    image = np.expand_dims(np.array(image)/255,0)
+
+    hasil = loaded_model.predict(image)
+    idx = hasil.argmax()
+    return labels[idx]
+
+
+#FASTAPI
+app = FastAPI()
+
+@app.post("/predict/image")
+async def predict_fastapi(file: UploadFile = File(...)):
+    image = process(await file.read())
+    prediction = predict(image)
+    return prediction
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
+
